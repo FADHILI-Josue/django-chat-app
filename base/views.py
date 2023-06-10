3c0a4c429d8232be5e40fa1d968d004cdc433c92
Christian-pprogrammer <mpanoc6@gmail.com>
2023-06-10 09:16:23 +0200

    making the message model

--- base/views.py
+++ base/views.py
@@ -6,24 +6,12 @@ from django.http import HttpResponse
 
 from .models import Room
 
-rooms = [
-    {'id':1, 'name': 'backend develpers'},
-    {'id':2, 'name': 'font end developers'},
-    {'id':3, 'name': 'fullstack developers'},
-    {'id':4, 'name': 'designers'}
-]
-
 def home(request):
     rooms = Room.objects.all()
     return render(request, 'base/home.html', {'rooms': rooms})
 
 def room(request,pk):
-    room = None
-
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-        context = {'room':room}
+    room = Room.objects.get(id=pk)
 
     return render(request, 'base/room.html', context)
     # return HttpResponse('room page')
